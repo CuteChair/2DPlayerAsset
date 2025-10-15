a995e81a098d9708f509124bde6dffcca68c6b78
Jules <jules.lhp@gmail.com>
2025-10-15 14:57:16 -0400

    PlayerAnimationManager
    Subscribed the methods to the corresponding events

--- Assets/Scripts/Player/PlayerAnimationManager.cs
+++ Assets/Scripts/Player/PlayerAnimationManager.cs
@@ -10,9 +10,47 @@ public class PlayerAnimationManager : MonoBehaviour
         p_animator = GetComponent<Animator>();
     }
 
+    private void OnEnable()
+    {
+        PlayerMovements.OnIdleEvent += TriggerIdleAnimation;
+        PlayerMovements.OnRunEvent  += TriggerRunAnimation;
+        PlayerMovements.OnJumpEvent += TriggerJumpAnimation;
+        PlayerMovements.OnFallEvent += TriggerFallAnimation;
+    }
+
+    private void OnDisable()
+    {
+        PlayerMovements.OnIdleEvent -= TriggerIdleAnimation;
+        PlayerMovements.OnRunEvent  -= TriggerRunAnimation;
+        PlayerMovements.OnJumpEvent -= TriggerJumpAnimation;
+        PlayerMovements.OnFallEvent -= TriggerFallAnimation;
+    }
+
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void TriggerIdleAnimation()
+    {
+        p_animator.SetBool("isRunning", false);
+        p_animator.SetBool("isFalling", false);
+    }
+    private void TriggerRunAnimation()
+    {
+        p_animator.SetBool("isFalling", false);
+        p_animator.SetBool("isRunning", true);
+    }
+    private void TriggerJumpAnimation()
+    {
+        p_animator.SetTrigger("Jump");
+        p_animator.SetBool("isFalling", false);
+        p_animator.SetBool("isRunning", false);
+    }
+    private void TriggerFallAnimation()
+    {
+        p_animator.SetBool("isFalling", true);
+        p_animator.SetBool("isRunning", false);
     }
 }
